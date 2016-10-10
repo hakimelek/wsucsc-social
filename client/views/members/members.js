225f7d54da0a4656a8bf0a328810afa7d77e09a3
Malek Hakim <hakimelek@gmail.com>
2016-10-10 17:59:16 -0500

    Permission to set passwords for Admin

--- client/views/members/members.js
+++ client/views/members/members.js
@@ -14,20 +14,18 @@ Template.members.helpers({
   },
 
   isMeOrAdmin: function () {
-    return this._id === Meteor.userId() || Roles.userIsInRole(Meteor.user(),
-                            ['admin', 'officer']);
+    return this._id === Meteor.userId() || Roles.userIsInRole(Meteor.user(), ['admin', 'officer']);
   },
 
   role: function () {
-    if (Roles.userIsInRole(this._id, ['admin'])) return "Admin";
-    else if (Roles.userIsInRole(this._id, ['officer']) && !Roles.userIsInRole(this._id, ['admin'])) return "Officer";
+    if (Roles.userIsInRole(this._id, ['admin'])) return 'Admin';
+    else if (Roles.userIsInRole(this._id, ['officer']) && !Roles.userIsInRole(this._id, ['admin'])) return 'Officer';
   },
 
   userIsOfficer: function () {
     return Roles.userIsInRole(this._id, ['admin']);
   }
 });
-
 
 Template.members.onCreated(function () {
   var self = this;
@@ -41,7 +39,7 @@ Template.members.onCreated(function () {
 Template.showMember.helpers({
   'member': function () {
     var controller = Router.current();
-    return Meteor.users.findOne({_id:  controller.params._id});
+    return Meteor.users.findOne({_id: controller.params._id});
   },
   isMe: function () {
     return this._id === Meteor.userId();
@@ -70,7 +68,7 @@ Template.showMember.onCreated(function () {
 Template.editMember.helpers({
   'member': function () {
     var controller = Router.current();
-    return Meteor.users.findOne({_id:  controller.params._id});
+    return Meteor.users.findOne({_id: controller.params._id});
   },
 
   isNotAdmin: function () {
@@ -78,13 +76,17 @@ Template.editMember.helpers({
                             ['admin']);
   },
 
+  isMeOrAdmin: function () {
+    return this._id === Meteor.userId() || Roles.userIsInRole(Meteor.user(), ['admin', 'officer']);
+  },
+
   isMe: function () {
     return this._id === Meteor.userId();
-  },
+  }
 });
 
 Template.editMember.events({
-  'submit .editMember': function(event, template) {
+  'submit .editMember': function (event, template) {
     event.preventDefault();
 
     var firstName = event.target.firstName.value;
@@ -92,9 +94,9 @@ Template.editMember.events({
     var email = event.target.email.value;
     var status;
 
-    if (event.target.status) {
+    if (event.target.status)
       status = event.target.status.value;
-    } else {
+    else {
       status = this.profile.status;
     }
 
@@ -103,7 +105,7 @@ Template.editMember.events({
     var profile = {
       'firstName': firstName,
       'lastName': lastName,
-      'status': status,
+      'status': status
     }
 
     Meteor.call('editMember', controller.params._id, profile, email, function (error, response) {
@@ -117,11 +119,11 @@ Template.editMember.events({
 
     var controller = Router.current();
 
-    swal({   title: "Are you sure?",   text: "You will not be able to recover this member!",   type: "warning",   showCancelButton: true,   confirmButtonColor: "#DD6B55",   confirmButtonText: "Yes, remove it!",   closeOnConfirm: false }, function(){
+    swal({ title: 'Are you sure?', text: 'You will not be able to recover this member!', type: 'warning', showCancelButton: true, confirmButtonColor: '#DD6B55', confirmButtonText: 'Yes, remove it!', closeOnConfirm: false }, function () {
       Meteor.call('removeMember', controller.params._id, function (error, response) {
         if (error) throw error;
         Router.go('members');
-        swal("Remove!", "The member has been removed.", "success");
+        swal('Remove!', 'The member has been removed.', 'success');
       });
     });
   },
@@ -133,7 +135,6 @@ Template.editMember.events({
       if (error) throw error;
       Router.go('members');
     });
-
   },
 
   'submit .setPassword': function (event, template) {
@@ -141,10 +142,10 @@ Template.editMember.events({
 
     // var oldPassword = event.target.oldpassword.value;
     var newPassword = event.target.newpassword.value;
+    var controller = Router.current();
 
-    Meteor.call('setPassword', newPassword, function (error, response) {
+    Meteor.call('setPassword', newPassword, controller.params._id, function (error, response) {
       if (error) throw error;
-
     });
   }
 });
@@ -164,7 +165,7 @@ Template.addMember.helpers({
 });
 
 Template.addMember.events({
-  'submit .addMember': function(event, template) {
+  'submit .addMember': function (event, template) {
     event.preventDefault();
 
     var firstName = event.target.firstName.value;
@@ -176,7 +177,7 @@ Template.addMember.events({
       'firstName': firstName,
       'lastName': lastName,
       'email': email,
-      'status': status,
+      'status': status
     }
 
     Meteor.call('addNewMember', user, function (error, response) {
@@ -190,17 +191,15 @@ Template.addMember.events({
 Template.addMember.onCreated(function () {
   var self = this;
   self.autorun(function () {
-
   });
 });
 
 AccountsTemplates.configure({
-    texts: {
-      title: {
-        signUp: "Become a member",
-      }
+  texts: {
+    title: {
+      signUp: 'Become a member'
     }
+  },
+
+  showForgotPasswordLink: true,
 });
-
-
-
